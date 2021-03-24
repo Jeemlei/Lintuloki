9c1af37c8659886c27411b3fe2ef92393bf6888e
Jeemlei <eemeli.ingervo@gmail.com>
2021-03-25 00:17:59 +0200

    Registration and login working with db

--- app/app.py
+++ app/app.py
@@ -1,6 +1,12 @@
-from flask import Flask, render_template, redirect, request
+from os import getenv
+from flask import Flask, render_template, redirect, request, session
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
+app.secret_key = getenv("SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv('SQLALCHEMY_DATABASE_URI')
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
@@ -9,11 +15,42 @@ def index():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
+        name = request.form['name']
+        username = request.form['username']
+        password = request.form['password']
+        psswrdhash = generate_password_hash(password)
+
+        try:
+            sql = 'INSERT INTO Users (realname, username, psswrdhash) VALUES (:name, :username, :psswrdhash)'
+            db.session.execute(sql, {'name':name,'username':username,'psswrdhash':psswrdhash})
+            db.session.commit()
+        except Exception as e:
+            print(e)
+            return redirect('/register')
+
         return redirect('/')
+        
     return render_template('register.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        return redirect('/')
+        username = request.form['username']
+        password = request.form['password']
+
+        sql = 'SELECT psswrdhash FROM users WHERE username=:username'
+        result = db.session.execute(sql, {'username':username})
+        user = result.fetchone()
+
+        if user:
+            session['username'] = username
+            return redirect('/')
+        else:
+            redirect('/login')
+
     return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    del session['username']
+    return redirect('/')
