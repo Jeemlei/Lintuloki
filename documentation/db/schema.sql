0e86cd9fc0c10efc4d046ae655309739594ec901
Jeemlei <eemeli.ingervo@gmail.com>
2021-03-29 02:21:37 +0300

    updated db schema

--- documentation/db/schema.sql
+++ documentation/db/schema.sql
@@ -1,21 +1,21 @@
 CREATE TABLE Users (
     id SERIAL PRIMARY KEY,
-    username VARCHAR(16) UNIQUE,
+    username VARCHAR(16) UNIQUE NOT NULL,
     realname VARCHAR(16),
-    psswrdhash TEXT
+    psswrdhash TEXT NOT NULL
 );
 
 CREATE TABLE Locations (
     id SERIAL PRIMARY KEY,
-    muni TEXT UNIQUE,
+    muni TEXT UNIQUE NOT NULL,
     prov TEXT,
-    country TEXT
+    country TEXT NOT NULL
 );
 
 CREATE TABLE Birds (
     id SERIAL PRIMARY KEY,
     sci TEXT UNIQUE NOT NULL,
-    fi TEXT NOT NULL,
+    fi TEXT UNIQUE NOT NULL,
     sv TEXT,
     en TEXT
 );
@@ -27,9 +27,9 @@ CREATE TABLE Observations (
     user_id INTEGER REFERENCES Users (id),
     location_id INTEGER REFERENCES Locations (id),
     bird_id INTEGER REFERENCES Birds (id),
-    bird_count INTEGER,
-    observation_date DATE,
-    banded banded_status,
+    bird_count INTEGER NOT NULL,
+    observation_date DATE NOT NULL,
+    banded banded_status NOT NULL,
     band_serial TEXT
 );
 
@@ -37,13 +37,14 @@ CREATE TABLE Comments (
     id SERIAL PRIMARY KEY,
     observation_id INTEGER REFERENCES Observations (id),
     user_id INTEGER REFERENCES Users (id),
-    content TEXT,
-    posting_date DATE NOT NULL DEFAULT CURRENT_DATE
+    content TEXT NOT NULL,
+    posting_time TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE Images (
     id SERIAL PRIMARY KEY,
     observation_id INTEGER REFERENCES Observations (id),
     user_id INTEGER REFERENCES Users (id),
-    data BYTEA
+    imagename TEXT NOT NULL,
+    binarydata BYTEA NOT NULL
 );
